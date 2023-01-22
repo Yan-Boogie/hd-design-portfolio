c6a3a2f54f6bb69f8c77d737725e1168abe147c6
Boogie-Yan <ian24257587@gmail.com>
2023-01-23 07:36:10 +0800

    build: set storybook env

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,16 @@
+import customTheme from '../theme';
+
 export const parameters = {
+  chakra: { theme: customTheme },
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  backgrounds: {
+    default: 'brand',
+    values: [{ name: 'brand', value: '#fef7de' }],
   },
 }
