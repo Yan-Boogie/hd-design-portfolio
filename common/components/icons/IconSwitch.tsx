8d2a17d2aae7385817dbd78fa3a766e011c223e6
Boogie-Yan <ian24257587@gmail.com>
2023-01-28 15:24:40 +0800

    feat: build IconSwitch

---
+++ common/components/icons/IconSwitch.tsx
@@ -0,0 +1,37 @@
+import { forwardRef } from '@chakra-ui/react';
+
+import IconMotion, { IconMotionProps } from './IconMotion';
+import IconUI, { IconUIProps } from './IconUI';
+
+type IsIconUIProps = (el: any) => el is IconUIProps;
+const isIconUIProps: IsIconUIProps = (el) => (el.motionType === 'none');
+
+export type IconSwitchProps = { motionType: 'full' | 'none' | 'half' } & (IconMotionProps | IconUIProps);
+
+const IconSwitch = forwardRef<IconSwitchProps, 'svg'>((props, ref) => {
+    if (isIconUIProps(props)) {
+        const { fontSize = '6xl', ...rest } = props;
+
+        return (
+            <IconUI fontSize={fontSize} ref={ref} {...rest} />
+        );
+    }
+
+    const { fontSize = '6xl', motionType, pathBundle, ...rest } = props;
+
+    if (motionType === 'half') return (
+        <IconMotion
+            fontSize={fontSize}
+            pathBundle={[pathBundle[pathBundle.length - 1]]}
+            {...rest} />
+    )
+
+    return (
+        <IconMotion
+            fontSize={fontSize}
+            pathBundle={pathBundle}
+            {...rest} />
+    );
+});
+
+export default IconSwitch;
--- common/components/icons/svgs/carouselRight.tsx
+++ common/components/icons/svgs/carouselRight.tsx
@@ -1,19 +1,19 @@
 import { forwardRef } from '@chakra-ui/react';
 
-import IconMotion, { IconMotionProps } from '../IconMotion';
-import IconUI, { IconUIProps } from '../IconUI';
+import IconSwitch, { IconSwitchProps } from '../IconSwitch';
 
 const InactivePath = () => (
     <>
         <path
-            fill="#fff"
+            fill="var(--chakra-colors-iconColors-primary)"
+            strokeWidth="2"
             d="M135.06 43.9l88.59 88.59-88.59 88.59H46.47l88.59-88.59L46.47 43.9h88.59m1.24-3H39.23l5.12 5.12 86.47 86.47-86.47 86.48-5.12 5.12h97.08l.88-.88 88.59-88.59 2.12-2.12-2.12-2.12-88.6-88.6-.88-.88z"
         />
         <path
             fill="none"
-            stroke="#fff"
+            stroke="var(--chakra-colors-iconColors-primary)"
             strokeMiterlimit="10"
-            strokeWidth={3}
+            strokeWidth="2"
             d="M135.06 43.9L135.06 221.09"
         />
     </>
@@ -22,48 +22,24 @@ const InactivePath = () => (
 const ActivePath = () => (
     <>
         <path
-            fill="#fef7de"
+            fill="var(--chakra-colors-brand-primary)"
+            strokeWidth="2"
+            stroke="var(--chakra-colors-iconColors-primary)"
             d="M135.06 132.5L135.06 43.9 46.47 43.9 135.06 132.5zM135.06 221.09L135.06 132.5 46.47 221.09 135.06 221.09z"
         />
         <path
-            fill="#fcc881"
+            fill="var(--chakra-colors-iconColors-primary)"
             d="M223.65 132.5L135.06 43.9 135.06 221.09 223.65 132.5z"
         />
     </>
 );
 
-/**
- * @todos
- * - Refactor the features to index.tsx
- */
-type IsIconMotionProps = (el: any) => el is IconUIProps;
-
-const isIconMotionProps: IsIconMotionProps = (el) => (el.motionType === 'none');
-
-export type CarouselRightProps = { motionType: 'full' | 'none' | 'half' } & (Omit<IconMotionProps, 'pathBundle'> | IconUIProps);
-
-export const CarouselRight = forwardRef<CarouselRightProps, 'div' | 'svg'>((props, ref) => {
-    if (isIconMotionProps(props)) {
-        const { fontSize = '6xl', ...rest } = props;
-        
-        return (
-            <IconUI fontSize={fontSize} ref={ref} {...rest} />
-        );
-    }
-
-    const { fontSize = '6xl', motionType, ...rest } = props;
-
-    if (motionType === 'half') return (
-        <IconMotion
-            fontSize={fontSize}
-            viewBox='0 0 270.12 264.99'
-            pathBundle={[<ActivePath key="active" />]}
-            {...rest} />
-    );
+export const CarouselRight = forwardRef<IconSwitchProps, 'div' | 'svg'>((props, ref) => {
+    const { ...rest } = props;
 
     return (
-        <IconMotion
-            fontSize={fontSize}
+        <IconSwitch
+            ref={ref}
             viewBox='0 0 270.12 264.99'
             pathBundle={[<InactivePath key="inactive" />, <ActivePath key="active" />]}
             {...rest} />
